0bee935371546c7a130eedc912c3410f60c9cd38
Sushilkushwah14 <sushilkushwah1411@gmail.com>
2024-06-26 12:45:42 +0530

    uploading 26/6/24

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -5,6 +5,7 @@ const ItemList=({items})=>{
       
           {items.map((item)=>
           <div  key={item.card.info.id} className="p-2 m-2  border-gray-400 border-b-4 text-left">
+            
             <div className="p-2">
             <span>{item.card.info.name}</span>
             <span>- ₹ {item.card.info.price?item.card.info.price/100:item.card.info.defaultPrice/100}</span>
